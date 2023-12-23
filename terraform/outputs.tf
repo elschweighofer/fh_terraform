09915d156078e4390d45b64bf3d4ade096385878
Lukas Schweighofer <lukas.schweighofer@outlook.at>
2023-12-23 21:46:32 +0100

    changes

---
+++ terraform/outputs.tf
@@ -0,0 +1,25 @@
+/* output "function_app_name" {
+  value = azurerm_linux_function_app.translator-function-app.name
+  description = "Deployed function app name"
+}
+
+output "function_app_default_hostname" {
+  value = azurerm_linux_function_app.translator-function-app.default_hostname
+  description = "Deployed function app hostname"
+}
+*/
+
+output "azurerm_cognitive_account_endpoint" {
+  value = azurerm_cognitive_account.TextAnalytics.endpoint
+  description = "Endpoint of the Text Analytics Service"
+}
+output "azurerm_cognitive_account_primary_key" {
+    value = azurerm_cognitive_account.TextAnalytics.primary_access_key
+    description = "Primary access key"
+    sensitive = true
+}
+output "azurerm_cognitive_account_key_secondary" {
+    value = azurerm_cognitive_account.TextAnalytics.secondary_access_key
+    description = "Primary access key"
+    sensitive = true
+}
