5ebe0c01e8a44f701d94baf83eb74e37e5589889
Lukas Schweighofer <lukas.schweighofer@outlook.at>
2023-12-25 08:18:27 +0100

    add defaults

--- variables.tf
+++ variables.tf
@@ -2,15 +2,18 @@
 
 variable "project" {
   type        = string
+  default = "translator"
   description = "Project name"
 }
 
 variable "environment" {
   type        = string
+  default = "dev"
   description = "Environment (dev / stage / prod)"
 }
 
 variable "location" {
   type        = string
+  default = "West Europe"
   description = "Azure region to deploy module to"
 }
