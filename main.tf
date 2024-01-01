5bce804d4b80f74162a63497801616a9e050b72e
Lukas Schweighofer <lukas.schweighofer@outlook.at>
2024-01-01 21:40:44 +0100

    fmt

--- main.tf
+++ main.tf
@@ -66,11 +66,11 @@ resource "azurerm_function_app" "vscode-function-2" {
 }
 
 resource "azurerm_application_insights" "insights" {
-  name = "${var.project}-${var.environment}-insights"
-  location = azurerm_resource_group.rg.location
+  name                = "${var.project}-${var.environment}-insights"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  application_type = "other"
-  
+  application_type    = "other"
+
 }
 # zip the source
 # https://xebia.com/blog/deploying-an-azure-function-with-terraform/
