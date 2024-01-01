6850508f41b1243302ae5a13889b6a404d2fc232
Lukas Schweighofer <lukas.schweighofer@outlook.at>
2024-01-01 22:03:24 +0100

    changes

--- main.tf
+++ main.tf
@@ -107,3 +107,35 @@ resource "azurerm_storage_blob" "storage_blob_function" {
   content_md5            = data.archive_file.function.output_md5
   source                 = "${path.module}/azure_function.zip"
 }
+
+
+resource "azurerm_postgresql_server" "psql-server" {
+  name                = "${var.project}-${var.environment}-psql-server"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku_name = "B_Gen5_2"
+
+  storage_mb                   = 5120
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+  auto_grow_enabled            = true
+
+  administrator_login          = "psqladmin"
+  administrator_login_password = "H@Sh1CoR3!"
+  version                      = "9.5"
+  ssl_enforcement_enabled      = true
+}
+
+resource "azurerm_postgresql_database" "psql-db" {
+  name                = "${var.project}-${var.environment}-psql-db"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_postgresql_server.psql-server.name
+  charset             = "UTF8"
+  collation           = "English_United States.1252"
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+}
